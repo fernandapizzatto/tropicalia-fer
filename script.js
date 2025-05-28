0136556701be659bf0bdd47b82869f352106ede5
fernandapizzatto <160729519+fernandapizzatto@users.noreply.github.com>
2025-05-28 11:34:40 +0000

    finalizamos o site

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
     const alternaContraste = document.getElementById('alterna-contraste')
+    const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+    botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
 
 
     let tamanhoAtualFonte = 1;
@@ -26,5 +28,8 @@ document.addEventListener('DOMContentLoaded', function(){
        alternaContraste.addEventListener('click', function(){
          document.body.classList.toggle('alto-contraste')
  });
-
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
 });
